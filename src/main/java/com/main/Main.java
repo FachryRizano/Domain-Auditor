eabe060403ff24a9f35e0e6ac0136afe43579b30
FachryRizano <fachry.rizano@gmail.com>
2020-10-07 13:58:22 +0700

    menambahkan fitur export ke output.txt

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -6,12 +6,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.Buffer;
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -22,23 +17,31 @@ public class Main {
         XSSFSheet sheet = xw.getSheetAt(0);
         Iterator<Row> itr = sheet.iterator();
         int i=0;
+
+
         while (itr.hasNext()) {
-            i++;
             List<Domain> domains = new ArrayList<>();
+            i++;
             String domainName = sheet.getRow(i).getCell(0).getStringCellValue();
             double pageAuthority = sheet.getRow(i).getCell(1).getNumericCellValue();
             double domainAuthority = sheet.getRow(i).getCell(2).getNumericCellValue();
             double spamScore = sheet.getRow(i).getCell(8).getNumericCellValue();
             double trustFlow = sheet.getRow(i).getCell(9).getNumericCellValue();
             double citationFlow = sheet.getRow(i).getCell(10).getNumericCellValue();
-            boolean isNull = spamScore == 0;
             if (pageAuthority >= 20 && domainAuthority >= 20 && spamScore <= 25 && trustFlow >= 15 && citationFlow >= 15) {
                 domains.add(new Domain(domainName,pageAuthority,domainAuthority,spamScore,trustFlow,citationFlow));
             }
-            for (Domain domain:domains) {
-                System.out.println(domain.getDomainName());
+
+            FileWriter writer = new FileWriter("output.txt",true);
+            BufferedWriter bw = new BufferedWriter(writer);
+
+            for (Domain domain:domains){
+                bw.write(domain.getDomainName());
+                bw.newLine();
             }
+            bw.close();
         }
+
         }catch(NullPointerException e){
             System.err.println(e);
             return;
